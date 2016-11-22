1c6226ebce03cf78e92b1c8b21e4213a64632b99
inexpensive <lawrence.bouzane@gmail.com>
2016-11-22 19:17:29 -0330

    added a simple UI

--- getFrequencies.py
+++ getFrequencies.py
@@ -1,3 +1,9 @@
+from Tkinter import Label
+from Tkinter import Tk
+from Tkinter import Entry
+from Tkinter import Button
+from Tkinter import StringVar
+from Tkinter import W
 import collections
 
 def generateNGram(n, fileName, nGram = {}):
@@ -23,7 +29,7 @@ def generateNGram(n, fileName, nGram = {}):
 
 	return nGram
 
-def getBestNMatches(nGram, n, *args):
+def getBestNMatches(nGram, n, args):
 	tup = tuple(args)
 	if nGram.has_key(tup):
 		words = nGram[tup]
@@ -31,6 +37,33 @@ def getBestNMatches(nGram, n, *args):
 		return sortedWords[0:n]
 	return None
 
+def matchEntry():
+    entry = matchThis.get().split()
+    words =  getBestNMatches(nGram, 6, entry)
+    out = ""
+    for word in words:
+        out += word
+        out += ", "
+    out = out[:-2]
+    result.set(out)
+
+
 nGram = generateNGram(3, "sampleText.txt")
 
-print getBestNMatches(nGram, 6, "to", "the")
+ui = Tk()
+
+Label(ui, text = "Enter text:").grid(row = 0)
+
+matchThis = Entry(ui)
+matchThis.grid(row = 0, column = 1)
+
+Button(ui, text = "Submit", command = matchEntry).grid(row = 3, column = 0, sticky = W, pady = 4)
+
+result = StringVar()
+result.set("")
+Label(ui, textvariable = result).grid(row = 1)
+
+
+#print getBestNMatches(nGram, 6, "to", "the")
+
+ui.mainloop()
