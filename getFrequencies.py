fd6a9f4988b69e948da3c249b9ba0b2eea31af3a
inexpensive <lawrence.bouzane@gmail.com>
2016-11-22 18:53:23 -0330

    adam's changes

--- getFrequencies.py
+++ getFrequencies.py
@@ -1,30 +1,36 @@
-testFile = file("sampleText.txt", "r")
-word1 = None
-word2 = None
-word3 = None
-frequencies = dict()
-for line in testFile:
-    for word in line.split():
-        #convert to lower case
-        word = word.lower()
-        #remove punctuation
-        word = filter(lambda ch: ch not in ",.()\'\"!?[]{}_-+=/<>|\\~`@#$%^&*;:", word)
-        if word1 == None:
-            word1 = word
-        elif word2 == None:
-            word2 = word
-        elif word3 == None:
-            word3 = word
-        else:
-            word1 = word2
-            word2 = word3
-            word3 = word
-        if word3 != None:
-            tup = (word1, word2, word3)
-            if tup not in frequencies:
-                frequencies[tup] = 1
-            else:
-                frequencies[tup] += 1
+import collections
 
-for key in frequencies:
-    print (str(key) + " " + str(frequencies[key]))
+def generateNGram(n, fileName, nGram = {}):
+	testFile = file(fileName, "r")
+	wordDeque = collections.deque(maxlen=n-1)
+	for line in testFile:
+		for word in line.split():
+			#convert to lower case
+			word = word.lower()
+			#remove punctuation
+			word = filter(lambda ch: ch not in ",.()\'\"!?[]{}_-+=/<>|\\~`@#$%^&*;:", word)
+
+			if len(wordDeque) == 2:
+				tup = tuple(wordDeque)
+				if not nGram.has_key(tup):
+					nGram[tup] = {}
+				if not nGram[tup].has_key(word):
+					nGram[tup][word] = 0
+				nGram[tup][word] += 1
+				wordDeque.popleft()
+
+			wordDeque.append(word)
+
+	return nGram
+
+def getBestNMatches(nGram, n, *args):
+	tup = tuple(args)
+	if nGram.has_key(tup):
+		words = nGram[tup]
+		sortedWords = sorted(words, words.get)
+		return sortedWords[0:n]
+	return None
+
+nGram = generateNGram(3, "sampleText.txt")
+
+print getBestNMatches(nGram, 6, "to", "the")
