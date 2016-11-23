a08feed768da05d8c8176b94f8086b6a6df587c2
inexpensive <lawrence.bouzane@gmail.com>
2016-11-23 18:18:45 -0330

    converted to create and load .ng (ngram) files to avoid having to constantly generate the ngrams

--- getFrequencies.py
+++ getFrequencies.py
@@ -1,4 +1,5 @@
-import collections
+import cPickle
+import os
 from Tkinter import END
 from Tkinter import Entry
 from Tkinter import IntVar
@@ -6,49 +7,19 @@ from Tkinter import Label
 from Tkinter import OptionMenu
 from Tkinter import StringVar
 from Tkinter import Tk
-import string
-import os
 
-def generate_ngram(n, file_name, ngram=None):
-    """Create an n-gram frequency dictionary based on the data in the file.
 
-    :param n: the size of the desired n-grams
-    :param file_name: the file of the text file to be analyzed for n-gram frequency
-    :param ngram: the dictionary to be appended to (defaults to {})
-    :return: a dictionary that encodes frequencies of different n-grams
+# noinspection PyUnusedLocal
+def load_n_gram_dict(*args):
+    """sets the current nGram to the one defined in the options
+
+    :param args: where the function was triggered (not used)
+    :return: nothing
     """
-    # open the file
-    if ngram is None:
-        ngram = {}
-    test_file = file(file_name, "r")
-    # start a deque to store the words
-    word_deque = collections.deque(maxlen=n - 1)
-    # run through each line in the file
-    for line in test_file:
-        # and each word in the line
-        for word in line.split():
-            # convert to lower case
-            word = word.lower()
-            # remove punctuation and non ASCII characters
-            printable = set(string.printable)
-            word = filter(lambda ch: ch not in ",.()'\"!?[]{}_-+=/<>|\\~`@#$%^&*;:" and ch in printable, word)
-            # check if n-1 words are in the deque
-           
-            if len(word_deque) == n - 1:
-                # if so, check if the deque is a key in the dictionary, if not create a dictionary as its value
-                tup = tuple(word_deque)
-                if tup not in ngram:
-                    ngram[tup] = {}
-                # then check if the inner dictionary has the next word as a key, if not, initialize it
-                if word not in ngram[tup]:
-                    ngram[tup][word] = 1
-                # increase the count and pop out the left-most (oldest) word
-                ngram[tup][word] += 1
-                word_deque.popleft()
-            # add the new word to the deque
-            word_deque.append(word)
-    # return the n-gram dictionary
-    return ngram
+    # convert the choices into the proper path and load the nGram associated with it
+    global nGram
+    n_gram_path = "text_sources/" + fileName.get() + "_{0}.ng".format(size.get() + 1)
+    nGram = cPickle.load(open(n_gram_path, "rb"))
 
 
 def get_best_m_matches(n_gram, m, inputted_words):
@@ -76,42 +47,44 @@ def match_entry(*args):
     :param args: where the function was triggered (not used)
     :return: nothing
     """
-    #clear the result labels first
-    for i in range(len(result)):
-        result[i].set("")
+    # clear the result labels first
+    for j in range(len(result)):
+        result[j].set("")
     # split the input into a list and find the best 3 entries
     entry = matchThis.get().split()
     words = get_best_m_matches(nGram, 3, entry)
     # if a non-None result is return, update the label to be those words
     if words is not None:
-        i = 0
+        j = 0
         for word in words:
-            result[i].set(word)
-            i += 1
+            result[j].set(word)
+            j += 1
 
 
-# noinspection PyUnusedLocal
-def make_new_n_gram_dict(*args):
-    """Makes a new n-gram dictionary when a new option in the option menus is selected
-
-    :param args: from where the function was triggered (not used)
-    :return: nothing
-    """
-    global nGram
-    nGram = {}
-    nGram = generate_ngram(size.get() + 1, textDirectory + fileName.get())
-
-
-def insert_text(i):
+def insert_text(j):
     """Inserts the chosen text into the entry box
 
-    :param i: the index of the result to be added
+    :param j: the index of the result to be added
     :return: nothing
     """
-    matchThis.insert(END, " " + result[i].get())
+    matchThis.insert(END, " " + result[j].get())
 
 
-# start with an empty n-gram dictionary
+def convert_to_readable_format(file_name_options):
+    """converts the given file name options list to a list with only the source names
+
+    :param file_name_options: the file name options that needs to be converted
+    :return: the shortened file name options list
+    """
+    # set up the output list
+    out = []
+    for option in sorted(file_name_options)[::5]:
+        out.append(option[:-5])
+
+    return out
+
+
+# initialize the nGram variable
 nGram = {}
 
 # set up the ui frame and give it a title
@@ -121,7 +94,7 @@ root.wm_title("Text Predictor")
 # set up a label prompting the user to enter text
 Label(root, text="Enter text:").grid(row=0)
 
-#set up as string variable to call the match entry function whenever it is changed
+# set up as string variable to call the match entry function whenever it is changed
 enteredText = StringVar()
 enteredText.set("")
 enteredText.trace("w", match_entry)
@@ -134,10 +107,10 @@ matchThis.grid(row=0, column=1)
 result = []
 for i in range(3):
     result.append(StringVar())
-    result[i].set("test")
+    result[i].set("")
     label = Label(root, textvariable=result[i])
     label.grid(row=1, column=i)
-    label.bind("<Button-1>", lambda e, i=i:insert_text(i))
+    label.bind("<Button-1>", lambda e, j=i: insert_text(j))
 
 # the n-gram size options (choice of n-1) this allows you to choose how many words the dict is keyed on
 sizeOptions = [
@@ -150,25 +123,27 @@ sizeOptions = [
 
 # set up the int variable that holds the size of n-1, and set its default value to the first option in sizeOptions
 size = IntVar()
-size.set(sizeOptions[0] + 1)
-# set a trace on this variable that calls make_new_n_gram_dict on any change
-size.trace("w", make_new_n_gram_dict)
+size.set(sizeOptions[0])
+# set a trace on this variable that calls load_n_gram_dict on any change
+size.trace("w", load_n_gram_dict)
 
-#directory which contain texts
+# directory which contain texts
 textDirectory = "text_sources/"
 
-#get files in diretory for options
+# get files in directory for options
 fileNameOptions = os.listdir(textDirectory)
+
+fileNameOptions = convert_to_readable_format(fileNameOptions)
 
 # set up the variable that holds the domain file, and set its default value to the first option in fileNameOptions
 fileName = StringVar()
 fileName.set(fileNameOptions[1])
 
-# set a trace on this variable that calls make_new_n_gram_dict on any change
-fileName.trace("w", make_new_n_gram_dict)
+# set a trace on this variable that calls load_n_gram_dict on any change
+fileName.trace("w", load_n_gram_dict)
 
 # create a new n_gram_dict based on the default size and domain
-make_new_n_gram_dict()
+load_n_gram_dict()
 
 # create the menu that holds the size options
 sizeMenu = apply(OptionMenu, (root, size) + tuple(sizeOptions))
