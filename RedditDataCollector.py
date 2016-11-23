1d648e38bab68b3d6ffe6827f4bbb89cfe53d175
Adam Eric Smith <aes702@prowl.cs.mun.ca>
2016-11-23 15:47:59 -0330

    reddit data collector

--- RedditDataCollector.py
+++ RedditDataCollector.py
@@ -1,12 +1,16 @@
 import sys
 sys.path.insert(0, 'praw/')
+sys.path.insert(0, 'pickle/')
 import praw
-
+import pickle
 
 p = praw.Reddit(user_agent='NLP Comment Scraper by /u/Smitttttty', client_id='1zFadZX5W60eXw', client_secret='TVSU9vn1wCzZKeum9qTCFUqpJB0')
 
 subreddit = p.subreddit("funny+news+AskReddit")
 
 #i will code something with this tomorrow
+f = open("test.txt", "w");
 for comment in subreddit.stream.comments():
-    print comment.body
+    if(len(comment.body) > 0):
+        f.write(comment.body + "\r\n")
+f.close();
